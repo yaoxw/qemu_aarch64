8afbcd37fe03f8731d8f7d0f6b27d876ea6a3a22
yaoxw <165671242@qq.com>
2018-06-03 17:09:43 +0800

    updated kernel config

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # qemu_aarch64
 Create qemu to simulate armv8 x64 system.
+
+1. qemu + uboot + kernel + ramdisk
+2. qemu + atf + uboot + kernel + ramdisk
--- build_kernel.sh
+++ build_kernel.sh
@@ -2,4 +2,9 @@
 cross_compile=/home/yxw/arm64/tool/gcc-linaro-6.3.1-2017.05-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
 make CROSS_COMPILE=$cross_compile ARCH=arm64 O=./out_aarch64 defconfig
 make CROSS_COMPILE=$cross_compile ARCH=arm64 O=./out_aarch64 menuconfig
-make CROSS_COMPILE=$cross_compile ARCH=arm64 O=./out_aarch64 Image -j4
+
+# need to enable ramdisk related config
+#	CONFIG_BLK_DEV_RAM CONFIG_BLK_DEV_INITRD
+#
+
+make CROSS_COMPILE=$cross_compile ARCH=arm64 O=./out_aarch64 Image -j8
