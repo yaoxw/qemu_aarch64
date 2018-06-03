6850577e9366eee6c1afdf49df61f1fa1f596462
yaoxw <165671242@qq.com>
2018-06-03 18:22:16 +0800

    update ramdisk build

--- make_ramdisk.sh
+++ make_ramdisk.sh
@@ -1,32 +1,35 @@
-#!/bin/bash
-sudo rm -rf rootfs
-sudo rm -rf tmpfs
-sudo rm -rf ramdisk*
-sudo mkdir rootfs
-sudo cp /home/yxw/arm64/busybox-1.28.4/_install/*  rootfs/ -raf
-sudo mkdir -p rootfs/proc/
-sudo mkdir -p rootfs/sys/
-sudo mkdir -p rootfs/tmp/
-sudo mkdir -p rootfs/root/
-sudo mkdir -p rootfs/var/
-sudo mkdir -p rootfs/mnt/
-sudo cp etc rootfs/ -arf
-sudo mkdir -p rootfs/lib
-sudo cp -arf /home/yxw/arm64/tool/gcc-linaro-6.3.1-2017.05-x86_64_aarch64-linux-gnu/aarch64-linux-gnu/libc/lib/* rootfs/lib/
-sudo rm rootfs/lib/*.a
-sudo /home/yxw/arm64/tool/gcc-linaro-6.3.1-2017.05-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-strip rootfs/lib/*
-sudo mkdir -p rootfs/dev/
-sudo mknod rootfs/dev/tty1 c 4 1
-sudo mknod rootfs/dev/tty2 c 4 2
-sudo mknod rootfs/dev/tty3 c 4 3
-sudo mknod rootfs/dev/tty4 c 4 4
-sudo mknod rootfs/dev/console c 5 1
-sudo mknod rootfs/dev/null c 1 3
-sudo dd if=/dev/zero of=ramdisk bs=1M count=16
-sudo mkfs.ext4 -F ramdisk
-sudo mkdir -p tmpfs
-sudo mount -t ext4 ramdisk ./tmpfs/  -o loop
-sudo cp -raf rootfs/*  tmpfs/
-sudo umount tmpfs
-sudo gzip --best -c ramdisk > ramdisk.gz
-sudo mkimage -n "ramdisk" -A arm -O linux -T ramdisk -C gzip -d ramdisk.gz ramdisk.img
+#!/bin/sh
+
+#########
+# build busybox
+#########
+cd busybox-1.28.4
+make menuconfig
+	# enable static bin and add  cross compiler prefix
+	# [*] Build busybox as a static binary
+	# Cross compiler prefix (/home/yxw/arm64/gcc-....)
+make
+make install
+
+#########
+# make ramdisk fs image
+#########
+cd busybox-1.28.4/_install
+mkdir proc sys dev etc etc/init.d
+cd etc/init.d
+echo > rcS
+echo "#!/bin/sh" > rcS
+echo "mount -t proc none /proc" >> rcS
+echo "mount -t sysfs none /sys" >> rcS
+echo "/sbin/mdev -s" >> rcS
+chmod 777 rcS
+
+cd ../..
+dd if=/dev/zero of=ramdisk.img bs=1M count=64
+mkfs.ext4 ramdisk.img
+mkdir tmpfs
+mount ramdisk.img tmpfs
+cd tmpfs
+cp -R busybox-1.28.4/_install/* tmpfs
+umount tmpfs
+
